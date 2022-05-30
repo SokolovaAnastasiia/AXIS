a5dff93fa438191c58260ceb247107232582d83b
SokolovaAnastasiia <aasokolova99@gmail.com>
2022-05-31 01:04:40 +0300

    отступ от сообщений

--- js/questions.js
+++ js/questions.js
@@ -219,7 +219,7 @@ let q = [];
   console.log(q);
 
 
-let mes_height = $(document).height() - 130;
+let mes_height = $(document).height() - 145;
 console.log(mes_height);
 $('.messages').height(mes_height);
 
